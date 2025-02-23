6468866adeabdb77d2fe1f14c77e4ada148c89bd
Söùrãv <souravpramanick1997@gmail.com>
2025-02-23 13:09:58 +0530

    blog and comment models updated

--- backend/models/blog.model.js
+++ backend/models/blog.model.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose")
 const blogSchema = new mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+    tags:[{type:String}],
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'comment' }],
   }, { timestamps: true });
 
   const BlogsModel = mongoose.model('blog', blogSchema);
--- backend/models/comment.model.js
+++ backend/models/comment.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const commentSchema = new mongoose.Schema({
   comment: { type: String, required: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
-  blog: { type: mongoose.Schema.Types.ObjectId, ref: 'blog', required: true },
+  blog_id: { type: mongoose.Schema.Types.ObjectId, ref: 'blog', required: true },
 }, { timestamps: true });
 
 const CommentModel= mongoose.model('comment', commentSchema);
