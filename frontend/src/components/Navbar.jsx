80638a91e7b9b9a2aa4beb4363b00bce2ae205c3
Söùrãv <souravpramanick1997@gmail.com>
2025-02-23 13:17:37 +0530

    Navbar updated with login based rendering

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,26 +6,26 @@ export default function Navbar() {
     const location = useLocation()
 
   return (
-    <nav className="fixed z-10 w-full left-0 top-0 bg-white shadow-2xl p-2 px-16 flex items-center justify-between">
+    <nav className="fixed z-10 w-full h-[8vh] left-0 top-0 bg-white shadow-md p-2 px-16 flex items-center justify-between">
         <NavLink to="/">
-      <div className="flex items-center">
-        <img src={logo} alt="logo" className="h-14" />
-        <p className="text-3xl text-primary font-extrabold">blogs</p>
+      <div className="flex items-center hover:scale-105 transition-transform duration-200 ease-in-out">
+        <img src={logo} alt="logo" className="h-12" />
+        <p className="text-[1.65rem] text-primary font-extrabold">blogs</p>
       </div>
         </NavLink>
-      <div className="flex items-center justify-between gap-4 text-lg">
+      <div className="flex items-center justify-between gap-6 text-lg">
 {   auth.token ?     <>
-        <NavLink to='/'><p className={`${location.pathname === '/' && 'text-primary'}`}>Home</p></NavLink>
-        <NavLink to='/profile'><p className={`${location.pathname === '/profile' && 'text-primary'}`}>Profile</p></NavLink>
-        <NavLink to='/create-blog'><p className={`${location.pathname === '/create-blog' && 'text-primary'}`}>Create Blog</p></NavLink>
+        <NavLink to='/'><p className={`${location.pathname === '/' && 'text-primary'} hover:scale-110 transition-transform duration-200 ease-in-out`}>Home</p></NavLink>
+        <NavLink to='/profile'><p className={`${location.pathname === '/profile' && 'text-primary'} hover:scale-110 transition-transform duration-200 ease-in-out`}>Profile</p></NavLink>
+        <NavLink to='/create-blog'><p className={`${location.pathname === '/create-blog' && 'text-primary'} hover:scale-110 transition-transform duration-200 ease-in-out`}>Create Blog</p></NavLink>
         </>
         :
         <>
         <NavLink to="/signin">
-        <button className="cursor-pointer bg-primary text-white p-1 px-5 rounded-3xl">Sing In</button>
+        <button className=" bg-primary text-white p-1 px-5 rounded-3xl hover:scale-105 transition-transform duration-200 ease-in-out">Sing In</button>
         </NavLink>
         <NavLink to="/signup">
-        <button className="cursor-pointer text-primary border border-primary p-1 px-5 rounded-3xl">Sing Up</button>
+        <button className=" text-primary border border-primary p-1 px-5 rounded-3xl hover:scale-105 transition-transform duration-200 ease-in-out">Sing Up</button>
         </NavLink>
         </>}
       </div>
