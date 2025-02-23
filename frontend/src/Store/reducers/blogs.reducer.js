d0008e6e4311e2229140af49deebb5e7ff852101
Söùrãv <souravpramanick1997@gmail.com>
2025-02-23 13:30:35 +0530

    allBlogs and myBlogs reducers added

---
+++ frontend/src/Store/reducers/blogs.reducer.js
@@ -0,0 +1,77 @@
+import {
+  GET_ALL_BLOGS_REQUEST,
+  GET_ALL_BLOGS_SUCCESS,
+  GET_ALL_BLOGS_ERROR,
+  LIKE_BLOG,
+  UNLIKE_BLOG,
+  POST_BLOG,
+  DELETE_BLOG,
+  // EDIT_BLOG,
+  GET_MY_BLOGS_REQUEST,
+  GET_MY_BLOGS_SUCCESS,
+  GET_MY_BLOGS_ERROR,
+} from "../action.types";
+const defaultState = {
+  isLoading: false,
+  isError: null,
+  blogs: [],
+};
+
+export function allBlogsReducer(state = defaultState, { type, payload }) {
+  switch (type) {
+    case GET_ALL_BLOGS_REQUEST:
+      return (state = { ...state, isLoading: true, isError: null });
+    case GET_ALL_BLOGS_SUCCESS:
+      return (state = { isError: null, isLoading: false, blogs: payload });
+    case GET_ALL_BLOGS_ERROR:
+      return (state = { ...state, isError: payload, isLoading: false });
+    case LIKE_BLOG: {
+      const blogs = state.blogs.map((blog) => {
+        if (blog._id.toString() === payload.blogId) {
+          blog.likes.push(payload.userId);
+          return blog;
+        } else {
+          return blog;
+        }
+      });
+      return (state = { isError: null, isLoading: null, blogs });
+    }
+    case UNLIKE_BLOG: {
+      const blogs = state.blogs.map((blog) => {
+        if (blog._id.toString() === payload.blogId) {
+          blog.likes = blog.likes.filter((userId) => userId !== payload.userId);
+
+          return blog;
+        } else {
+          return blog;
+        }
+      });
+      return (state = { isError: null, isLoading: null, blogs });
+    }
+    case POST_BLOG:
+      return (state = {
+        isError: null,
+        isLoading: false,
+        blogs: [payload, ...state.blogs],
+      });
+    default:
+      return state;
+  }
+}
+
+export function myBlogsReducer(state = defaultState, { type, payload }) {
+  switch (type) {
+    case GET_MY_BLOGS_REQUEST:
+      return (state = { ...state, isLoading: true, isError: null });
+    case GET_MY_BLOGS_SUCCESS:
+      return (state = { isError: null, isLoading: false, blogs: payload });
+    case GET_MY_BLOGS_ERROR:
+      return (state = { ...state, isError: payload, isLoading: false });
+    case DELETE_BLOG:{
+      const blogs = state.blogs.filter(blog=>blog._id!==payload)
+      return state = {blogs,isLoading:false,isError:null}
+    }
+    default:
+      return state;
+  }
+}
--- frontend/src/Store/store.js
+++ frontend/src/Store/store.js
@@ -2,8 +2,11 @@ import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import authReducer from "./reducers/auth.reducer";
 import {thunk} from 'redux-thunk'
 import snackbarReducer from "./reducers/snackbar.reducer";
+import {allBlogsReducer, myBlogsReducer} from "./reducers/blogs.reducer";
 const rootReducer = combineReducers({
     auth:authReducer,
-    snackbar:snackbarReducer
+    snackbar:snackbarReducer,
+    allBlogs:allBlogsReducer,
+    myBlogs:myBlogsReducer
 })
 export const store = legacy_createStore(rootReducer,applyMiddleware(thunk))
